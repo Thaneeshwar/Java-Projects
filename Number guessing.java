387e271f012c3434ccf4343fd52517964e4887a5
Thaneeshwar <smthaneeshwar@gmail.com>
2026-04-13 14:26:39 +0530

    Create Number guessing.java

---
+++ Number guessing.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int secretNumber = random.nextInt(100) + 1;
+        int attempts = 0;
+        int guess;
+        
+        System.out.println("Guess the number (1-100)!");
+        
+        while (true) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+            
+            if (guess < secretNumber) {
+                System.out.println("Too low!");
+            } else if (guess > secretNumber) {
+                System.out.println("Too high!");
+            } else {
+                System.out.println("Correct! You guessed it in " + attempts + " attempts!");
+                break;
+            }
+        }
+        scanner.close();
+    }
+}
